129d66a276c14306d82e4b41e069921f920fb79e
Cometores <anicometores@gmail.com>
2023-04-28 10:47:01 +0200

    Slider improvement

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './styles/App.css';
 import { header } from './mocks/header.ts';
 import Header from './components/Header/Header.tsx';
 import Gallery from './components/Gallery/Gallery.jsx';
-import Product from './components/UI/Product/Product.jsx';
-import PicSlider from './components/PicSlider/PicSlider.jsx';
+import Slider from './components/PicSlider/Slider.jsx';
 import CardCollection from './components/CardCollection/CardCollection.jsx'
+import slides from './img/Slider.json'
 
 export const App = () => {
   return (
     <div className="App">
       <Header items={header} />
       <CardCollection/>
-      <PicSlider />
+      <Slider slides={slides}/>
       <Gallery/>
     </div>
   );
---
+++ src/components/PicSlider/Slider.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import styles from './Slider.module.css';
+// import 'swiper/css'
+import 'swiper/swiper-bundle.min.css';
+
+const Slider = ({ slides }) => {
+
+	return (
+		<div>
+			<Swiper
+				slidesPerView={1}
+				loop
+				modules={[Navigation, Pagination, A11y, Autoplay]}
+				autoplay={{ delay:5000 }}
+				navigation
+				pagination={{ clickable: true }}
+			>
+				{slides.map((slide, index) => (
+					<SwiperSlide key={index}>
+						<img
+							className={styles.Image}
+							src={slide.image}
+							alt={slide.title} />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
+export default Slider;
